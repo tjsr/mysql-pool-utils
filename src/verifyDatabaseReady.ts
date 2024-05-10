a575f62b961754d4991e849a7801485dee206af4
Tim Rowe <tim@tjsr.id.au>
2024-05-10 20:29:26 +1000

    Increase delays and timeout when checking for MySQL server.

--- src/verifyDatabaseReady.ts
+++ src/verifyDatabaseReady.ts
@@ -1,10 +1,16 @@
 import { Connection, ConnectionOptions } from "mysql2/promise";
 
+import { intEnv } from "@tjsr/simple-env-utils";
 import { mysqlQuery } from "./mysqlQuery.js";
 import { persistentlyGetConnection } from "./persistentlyGetConnection.js";
 
+const databaseCheckRetries = intEnv('DATABASE_CHECK_RETRIES', 10);
+const databaseCheckDelay = intEnv('DATABASE_CHECK_DELAY', 200);
+const databaseTimeout = intEnv('DATABASE_TIMEOUT', databaseCheckDelay*(databaseCheckRetries+1));
+
 export const verifyDatabaseReady = async (connectionDetails: ConnectionOptions): Promise<void> => {
-  const connectionPromise: Promise<Connection> = persistentlyGetConnection(connectionDetails, 10, 200, 1000);
+  const connectionPromise: Promise<Connection> = persistentlyGetConnection(
+    connectionDetails, databaseCheckRetries, databaseCheckDelay, databaseTimeout);
   return new Promise<void>((resolve, reject) => {
     mysqlQuery('SELECT 1', [], connectionPromise).then(() => {
       return resolve();
